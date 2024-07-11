4fb4e62a2e1560eaaec2f1e263f852a6198c86ee
ddettaa <jangandihirani000@gmail.com>
2024-07-11 16:30:56 +0800

    add : Penambahan switch case

--- Perhitungan Pace.cpp
+++ Perhitungan Pace.cpp
@@ -5,12 +5,33 @@ using namespace std;
 
 int main() {
 
-  
+    int pilihan;
     do {
         cout << "=== Program Penghitung Pace Lari ===" << endl;
         cout << "1. Input data lari" << endl;
         cout << "2. Tampilkan hasil perhitungan" << endl;
         cout << "3. Keluar" << endl;
         cout << "Masukkan pilihan Anda: ";
-        return 0;
+        cin >> pilihan;
+
+        switch (pilihan) {
+            case 1: {
+                
+                cout << "" << endl;
+                break;
+            }
+            case 2: {
+                
+                cout << "" << endl;
+                break;
+            }
+            case 3:
+                cout << "aplikasi keluar" << endl;
+                break;
+            default:
+                cout << "Pilihan tidak valid. Silakan coba lagi." << endl;
+        }
+    } while (pilihan != 3);
+
+    return 0;
 }
