cd89b9307086d92b61713bf43ff5b85ed7285e9b
ddettaa <jangandihirani000@gmail.com>
2024-07-11 16:28:21 +0800

    add : Penambahan Menu awal

--- Perhitungan Pace.cpp
+++ Perhitungan Pace.cpp
@@ -1,7 +1,16 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+
+  
+    do {
+        cout << "=== Program Penghitung Pace Lari ===" << endl;
+        cout << "1. Input data lari" << endl;
+        cout << "2. Tampilkan hasil perhitungan" << endl;
+        cout << "3. Keluar" << endl;
+        cout << "Masukkan pilihan Anda: ";
+        return 0;
 }
